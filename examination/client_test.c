50a4c550ba00492043dd3cecab1e40a1d2365454
edvinas1122 <edvinasmomkus@gmail.com>
2021-11-27 18:11:16 +0100

    description

--- examination/client_test.c
+++ examination/client_test.c
@@ -6,18 +6,26 @@
 /*   By: emomkus <emomkus@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 23:09:38 by emomkus           #+#    #+#             */
-/*   Updated: 2021/11/27 01:09:10 by emomkus          ###   ########.fr       */
+/*   Updated: 2021/11/27 18:10:35 by emomkus          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <signal.h>
 
+/*
+	takes server PID and sends received string to PID
+*/
+
 int	main(int argv, char **args)
 {
 	int	i;
 
-	args[1][0]
-	args[2][0]
+	/*
+	args[1][0] - pointer to received PID 
+	args[2][0] - pointer to received string
+	*/
+
+	
 	return (0);
 }
--- examination/server_test.c
+++ examination/server_test.c
@@ -6,7 +6,7 @@
 /*   By: emomkus <emomkus@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 23:09:44 by emomkus           #+#    #+#             */
-/*   Updated: 2021/11/27 00:54:29 by emomkus          ###   ########.fr       */
+/*   Updated: 2021/11/27 18:10:01 by emomkus          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,21 @@
 #include <stdio.h>
 #include <unistd.h>
 
+/*
+	Display PID, waits for signal and prints received string 
+*/
+
 int	main(void)
 {
 	pid_t	id;
 	
-	id = getpid();
-	printf("Server pid: %d", id);
+	id = getpid(); /* gets - process id - (PID) */
+	printf("Server pid: %d", id); /* prints process id */
+
+	while (1)	/* endless loop checks for incoming signal */
+	{
+		sleep(1);
+		signal(SIGINT, handler);
+	}
 	return (0);
 }
